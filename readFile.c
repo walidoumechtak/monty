6d9ae51e2c97d0d634dff6c237357148a9c000be
walid oumechtak <woumechtak@gmail.com>
2023-09-19 08:58:32 -0700

    done

--- readFile.c
+++ readFile.c
@@ -7,5 +7,26 @@
 
 void readFile(data_t *data)
 {
-	
+	char buffer[1024];
+	int	read_size;
+	char *res;
+	char *temp;
+
+	res = NULL;
+	read_size = read(data->fd, buffer, 1024);
+	if (read_size < 0)
+		exit(EXIT_FAILURE);
+	buffer[read_size] = '\0';
+	res = _strjoin(buffer, res);
+	while (read_size > 0)
+	{
+		read_size = read(data->fd, buffer, 1024);
+		if (read_size < 0)
+			exit(EXIT_FAILURE);
+		buffer[read_size] = '\0';
+		temp = _strjoin(res, buffer);
+		free(res);
+		res = temp;
+	}
+	data->content = res;
 }
